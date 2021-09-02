ae9bffb7eed39c7f25b2113f6f8e7ba93439ac97
kelvinninja1 <kelvinninja1@gmail.com>
2021-09-02 06:32:30 -0400

    Added: The problem triangle

--- src/main/java/io/turntabl/exercises/TriangleMaxPathSum.java
+++ src/main/java/io/turntabl/exercises/TriangleMaxPathSum.java
@@ -11,6 +11,30 @@ package src.main.java.io.turntabl.exercises;
 // 8 5 9 3                   8 5 9 3            - 3.1 | 3.2*        [a+3][a+2] a-1
 
 // That is, 3 + 7 + 4 + 9 = 23.
+
+// Find the maximum total from top to bottom of the triangle below:
+
+// 75
+// 95 64
+// 17 47 82
+// 18 35 87 10
+// 20 04 82 47 65
+// 19 01 23 75 03 34
+// 88 02 77 73 07 63 67
+// 99 65 04 28 06 16 70 92
+// 41 41 26 56 83 40 80 70 33
+// 41 48 72 33 47 32 37 16 94 29
+// 53 71 44 65 25 43 91 52 97 51 14
+// 70 11 33 28 77 73 17 78 39 68 17 57
+// 91 71 52 38 17 14 91 43 58 50 27 29 48
+// 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+// 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+// NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. 
+
+// However, for a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method. 
+
+// See if you can think of both a brute force method and a cleverer way to solve it. 
  
 public class TriangleMaxPathSum
 {
@@ -98,11 +122,24 @@ public class TriangleMaxPathSum
 
         // Use Test rather
         int triangle[][] = { 
-                        { 3 },
-                        { 7, 4 },
-                        { 2, 4, 6 },
-                        { 8, 5, 9, 3 }
+                        {75},
+                        {95, 64},
+                        {17, 47, 82},
+                        {18, 35, 87, 10},
+                        {20, 04, 82, 47, 65},
+                        {19, 01, 23, 75, 03, 34},
+                        {88, 02, 77, 73, 07, 63, 67},
+                        {99, 65, 04, 28, 06, 16, 70, 92},
+                        {41, 41, 26, 56, 83, 40, 80, 70, 33},
+                        {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+                        {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+                        {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+                        {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+                        {63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+                        {04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23}
                     };
+
+                    // replace 9 with 09
  
         System.out.println(findMaxPath(triangle));
     }
